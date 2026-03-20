e37e5c32d8c2043a6bf04b25cf8f9b74cd130ba2
saba <tameemtakouan@hotmail.com>
2026-03-21 03:40:25 +0800

    Release v0.2.6 with sudoku v0.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ val gitRefName: String? = System.getenv("GITHUB_REF_NAME")
 val tagVersionName: String? = gitRefName
     ?.removePrefix("refs/tags/")
     ?.removePrefix("v")
-val computedVersionName: String = tagVersionName ?: "0.2.4"
+val computedVersionName: String = tagVersionName ?: "0.2.6"
 
 fun computeVersionCodeFromName(name: String): Int {
     val parts = name.split(".")
